276cf9a0ee07077074e3cb8f29f17475220c723e
付野 <834225691@qq.com>
2017-08-05 17:51:20 +0800

    Initial Commit

---
+++ FYRequsetDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FYRequsetDemo
+//
+//  Created by 付野 on 2017/8/5.
+//  Copyright © 2017年 com.halobear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FYRequsetDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FYRequsetDemo
+//
+//  Created by 付野 on 2017/8/5.
+//  Copyright © 2017年 com.halobear. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
